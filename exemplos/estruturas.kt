e0c2575b7a17d169f1d591d1464fbbc24907ce9a
Matheus Fernandes <154653138+thiprer497@users.noreply.github.com>
2026-02-25 04:08:48 -0800

    Update estruturas.kt

--- exemplos/estruturas.kt
+++ exemplos/estruturas.kt
@@ -3,31 +3,31 @@
  * Teste de estruturas sintáticas do Kotlin
  */
 
-package com.exemplo.estruturas
+package com.exemplo.estruturas;
 
 // Data Class e Construtor Primário
-data class Usuario(val id: Int, val nome: String)
+data class Usuario(val id: Int, val nome: String);
 
 // Interface e Generics
 interface Repositorio<T> {
-    fun buscar(id: Int): T?
-    fun salvar(item: T)
+    fun buscar(id: Int): T?;
+    fun salvar(item: T);
 }
 
 fun main() {
     // Listas e Lambdas
-    val numeros = listOf(1, 2, 3, 4, 5)
+    val numeros = listOf(1, 2, 3, 4, 5);
     
     // Teste do token SETA (->) e CHAVES ({ })
-    val dobrados = numeros.map { numero -> 
-        numero * 2 
-    }
+    val dobrados = numeros.map { numero ->
+        numero * 2;
+    };
 
     // Null Safety e Elvis Operator
-    val nulo: String? = null
-    val tamanho = nulo?.length ?: 0
+    val nulo: String? = null;
+    val tamanho = nulo?.length ?: 0;
 
     // Annotation (O Lexer lê @ como AT e depois o ID)
-    @Deprecated("Use a nova funcao")
-    fun antiga() { }
+    @Deprecated("Use a nova funcao");
+    fun antiga() { };
 }
