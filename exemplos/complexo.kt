d35db14deab5736308ccf246f5bb21200c43c929
Matheus Fernandes <154653138+thiprer497@users.noreply.github.com>
2026-02-25 04:09:13 -0800

    Update complexo.kt

--- exemplos/complexo.kt
+++ exemplos/complexo.kt
@@ -20,21 +20,21 @@ fun main() {
         SELECT * FROM "tabela"
         WHERE nome = 'O''Neal'
         AND id > 0
-    """
+    """;
 
     // 3. Ambiguidade de Operadores (Maximal Munch)
-    var a = 10
-    var b = 20
-    val soma = a+++b    // Deve ser lido como: a ++ + b
-    val range = 0..<10  // Operador rangeUntil
+    var a = 10;
+    var b = 20;
+    val soma = a+++b;    // Deve ser lido como: a ++ + b
+    val range = 0..<10;  // Operador rangeUntil
 
     // 4. Identificadores Soft Keywords (podem ser nomes de var)
-    val file = 1
-    val field = 2
-    val get = 3
-    val set = 4
+    val file = 1;
+    val field = 2;
+    val get = 3;
+    val set = 4;
     
     // 5. Unicode e Escapes
-    val unicodeChar = '\u00A9' // Copyright symbol
-    val citacao = "Ele disse: \"Olá!\""
+    val unicodeChar = '\u00A9'; // Copyright symbol
+    val citacao = "Ele disse: \"Olá!\"";
 }
